f69af025fb21893c6d16ba25ebd7861711734180
Riley Worstell <rcwors02@gmail.com>
2021-05-05 23:13:46 -0400

    file cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,7 @@ import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-form/CreateProfile';
 import Profiles from './components/profiles/Profiles';
 import EditProfile from './components/profile-form/EditProfile';
-import AddExperience from './components/profile-form/AddExperience';
 import Profile from './components/profile/Profile';
-import AddEducation from './components/profile-form/AddEducation';
 import Posts from './components/posts/Posts';
 import Post from './components/post/Post';
 import Admin from './components/admin/Admin';
@@ -29,7 +27,7 @@ if (localStorage.token) {
 }
 
 const App = () => { 
-  
+
   // similar to componentDidMount, the empty brackets as second param makes it only run once
   useEffect(() => {
     store.dispatch(loadUser());
@@ -55,8 +53,6 @@ const App = () => {
             <PrivateRoute exact path="/create-post" component= { CreatePost } />
             <PrivateRoute exact path="/create-profile" component= { CreateProfile } />
             <PrivateRoute exact path="/edit-profile" component= { EditProfile } />
-            <PrivateRoute exact path="/add-experience" component= { AddExperience } />
-            <PrivateRoute exact path="/add-education" component= { AddEducation } />
             <PrivateRoute exact path="/posts/:id" component= { Post } />
             {/* TODO add a /profile/:id */}
           </Switch>
